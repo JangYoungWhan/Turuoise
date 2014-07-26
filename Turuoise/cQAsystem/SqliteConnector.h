87364805b56940efe2049895f44d11e93beaab00
whwnsdlr1 <whwnsdlr1@naver.com>
2014-07-27 03:29:12 +0900

    함수추가
    getCountWordID()
    getSumTermFreq()
    getDocInfo(Integer doc_id, int flag)

--- Turuoise/cQAsystem/SqliteConnector.h
+++ Turuoise/cQAsystem/SqliteConnector.h
@@ -40,6 +40,9 @@ public:
 	Integer SqliteConnector::getTF(String &term, Integer doc, int flag);
 	Integer getDF(String &term, int flag);
 	Real getIDF(String &term, int flag);
+	Integer getCountWordID();
+	Integer getSumTermFreq();
+	const std::forward_list<Term<String, Integer>>* getDocInfo(Integer doc_id, int flag);
 
 private: // Utils
 	std::vector< std::vector< String>> SqliteConnector::queryDB(const char* query);
